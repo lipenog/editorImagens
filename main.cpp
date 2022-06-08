10b124e0185c2a9337a7a9ea8f59ddc431923a35
lipenog <106320475+lipenog@users.noreply.github.com>
2022-06-07 21:53:48 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -98,18 +98,26 @@ void writeImg(string nameFile, int rows, int col, int maxValue, int *img)
     myFile.close();
 }
 
-void negative(int rows, int col, int *img, int *imgResult)
+int *negative(int rows, int col, int *img)
 {
+    int *imgResult = new int[col*rows];
+    int *result = imgResult;
+
     for (int i = 0; i < rows * col; i++)
     {
         *imgResult = 255 - *img;
         imgResult++;
         img++;
     }
+
+    return result;
 }
 
-void levelOfLight(int rows, int col, int maxValue, int *img, int *imgResult, float levelOfLight)
+int *levelOfLight(int rows, int col, int maxValue, int *img, float levelOfLight)
 {
+    int *imgResult = new int[col*rows];
+    int *result = imgResult;
+
     for (int i = 0; i < rows * col; i++)
     {
         *imgResult = *img * levelOfLight;
@@ -127,90 +135,167 @@ void levelOfLight(int rows, int col, int maxValue, int *img, int *imgResult, flo
         imgResult++;
         img++;
     }
+
+    return result;
 }
 
-void transpose(int rows, int col, int *img, int *imgResult)
+int *transpose(int rows, int col, int *img)
 {
-    for (int i = 0; i < rows; i++)
+    int *imgResult = new int[col*rows];
+
+    int *p1 = imgResult;
+    int *p2 = img;
+    int *q1, *q2;
+    for (; p1 < imgResult + (col*rows) - rows; p1 += rows, p2++)
     {
-        for (int j = i; j < col; j++)
+        for (q1 = p1, q2 = p2; q1 <= p1 + rows; q1++, q2 += col)
         {
-            imgResult[col * i + j] = img[col * j + i];
-            imgResult[col * j + i] = img[col * i + j];
+            *q1 = *q2;
         }
     }
+
+    return imgResult;
 }
 
-void horizontalRotation(int rows, int col, int *img, int *imgResult)
+int *horizontalRotation(int rows, int col, int *img)
 {
+    int *imgResult = new int[col*rows];
+
     int *p1 = imgResult;
     int *p2 = img + (col * rows) - col;
+    int *q1, *q2;
 
-    for (int i = 0, j = 0; i < rows * col; i++, p1++, p2++, j++)
+    for (; p1 <= imgResult + (col * rows) - col; p1 += col, p2 -= col)
     {
-        *p1 = *p2;
-        if (j == col)
+        for (q1 = p1, q2 = p2; q1 <= p1 + col; q1++, q2++)
         {
-            p2 = p2 - 2 * col;
-            j = 0;
+            *q1 = *q2;
         }
     }
+
+    return imgResult;
 }
 
-void verticalRotation(int rows, int col, int *img, int *imgResult)
+int *verticalRotation(int rows, int col, int *img)
 {
+    int *imgResult = new int[col*rows];
+
     int *p1 = imgResult;
     int *p2 = img + col - 1;
+    int *q1, *q2;
 
-    for (int i = 0, j = 0; i < rows * col; i++, p1 += col, p2 += col, j++)
+    for (; p1 <= imgResult + col - 1; p1++, p2--)
     {
-        *p1 = *p2;
-        if (j == rows)
+        for (q1 = p1, q2 = p2; q1 <= p1 + (col * rows) - col; q1 += col, q2 += col)
         {
-            p1 = p1 - rows * col + col + 1;
-            p2 = p2 - rows * col + 2;
-            j = 0;
+            *q1 = *q2;
         }
     }
+
+    return imgResult;
 }
 
-void rightRotation()
+int *rightRotation(int rows, int col, int *img)
 {
+    int *result = new int[rows*col];
+    result = verticalRotation(col, rows, transpose(rows, col, img));
+    return result;
 }
 
-void leftRotation()
+int *leftRotation(int rows, int col, int *img)
 {
+    int *result = new int[rows*col];
+    result = horizontalRotation(col, rows, transpose(rows, col, img));
+    return result;
+}
+
+int *saltAndPepper(int rows, int col, int maxValue, int *img)
+{
+    int *imgResult = new int[rows*col];
+    for (int *p1 = imgResult; p1 <= imgResult + (col*rows); p1++, img++)
+    {
+        int v1 = rand() % 100;
+        if (v1 == 0)
+        {
+            *p1 = 0;
+        } else if (v1 == 10) {
+            *p1 = maxValue;
+        } else {
+            *p1 = *img;
+        }
+    }
+    return imgResult;
+}
+
+int *padding(int rows, int col, int maxValue, int *img, int dimension)
+{
+    int colResult = (col+2*dimension), rowsResult = (rows+2*dimension); 
+    int *imgResult = new int[colResult*rowsResult];
+    int *p1, *p2, *q1, *q2;
+    for (p1 = imgResult; p1 < imgResult + (colResult*rowsResult); p1++)
+    {
+        *p1 = maxValue;
+    } 
+  
+    for (p1 = imgResult + (dimension*colResult) + dimension, p2 = img; p1 < imgResult + (dimension*colResult) + dimension + col; p1++, p2++)
+    {
+        for (q1 = p1, q2 = p2; q1 < p1 + (colResult*rowsResult) - 2*dimension*colResult; q1 += colResult, q2 += col)
+        {
+            *q1 = *q2;
+        }
+    }
+
+    return imgResult;
 }
 
 int main(void)
 {
     int rows, col, maxValue;
-    getRowsCol("mulher.pgm", &rows, &col, &maxValue);
-
+    getRowsCol("totem.pgm", &rows, &col, &maxValue);
+    cout << rows << " " << col << endl;
     int *img = new int[rows * col];
     int *imgResult = new int[rows * col];
+    int *imgPadding = new int[(col+2)*(rows+2)];
+    int *imgPadding2 = new int[(col+4)*(rows+4)];
+    int *imgPadding3 = new int[(col+6)*(rows+6)];
 
-    readImg("mulher.pgm", rows, col, img);
+    readImg("totem.pgm", rows, col, img);
 
-    writeImg("teste.pgm", rows, col, maxValue, img);
-
-    negative(rows, col, img, imgResult);
+    imgResult = negative(rows, col, img);
     writeImg("negative.pgm", rows, col, maxValue, imgResult);
 
-    levelOfLight(rows, col, maxValue, img, imgResult, 1.8);
+    imgResult = levelOfLight(rows, col, maxValue, img, 1.8);
     writeImg("brighten.pgm", rows, col, maxValue, imgResult);
 
-    levelOfLight(rows, col, maxValue, img, imgResult, 0.2);
+    imgResult = levelOfLight(rows, col, maxValue, img, 0.2);
     writeImg("darken.pgm", rows, col, maxValue, imgResult);
 
-    transpose(rows, col, img, imgResult);
+    imgResult = transpose(rows, col, img);
     writeImg("transpose.pgm", col, rows, maxValue, imgResult);
 
-    horizontalRotation(rows, col, img, imgResult);
+    imgResult = horizontalRotation(rows, col, img);
     writeImg("horizontal.pgm", rows, col, maxValue, imgResult);
 
-    verticalRotation(rows, col, img, imgResult);
+    imgResult = verticalRotation(rows, col, img);
     writeImg("vertical.pgm", rows, col, maxValue, imgResult);
+
+    imgResult = rightRotation(rows, col, img);
+    writeImg("rightR.pgm", col, rows, maxValue, imgResult);
+
+    imgResult = leftRotation(rows, col, img);
+    writeImg("leftR.pgm", col, rows, maxValue, imgResult);
+
+    imgResult = saltAndPepper(rows, col, maxValue, img);
+    writeImg("salt&pepper.pgm", rows, col, maxValue, imgResult);
+
+    imgPadding = padding(rows, col, maxValue, img, 1);
+    writeImg("padding.pgm", rows+2, col+2, maxValue, imgPadding);
+
+    imgPadding2 = padding(rows, col, maxValue, img, 2);
+    writeImg("padding2.pgm", rows+4, col+4, maxValue, imgPadding2);
+
+    imgPadding3 = padding(rows, col, maxValue, img, 3);
+    writeImg("padding3.pgm", rows+6, col+6, maxValue, imgPadding3);
 
     return 0;
 }
